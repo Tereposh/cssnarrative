a89ec03c622f9474e92d565615b35fac58b7c113
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-02-20 15:22:04 +0000

    Update .DEPS.git

--- app/src/androidTest/java/com/pras/bareksatest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/pras/bareksatest/ExampleInstrumentedTest.kt
@@ -15,3 +15,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = In
