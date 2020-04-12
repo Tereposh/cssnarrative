e88c1c0a5e697a605dd616003e9cd96ee5b83b4e
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-04-12 07:15:58 +0000

    Added missing image

--- app/src/main/java/com/pras/bareksatest/model/local/entity/DataEntity.kt
+++ app/src/main/java/com/pras/bareksatest/model/local/entity/DataEntity.kt
@@ -1,1 +1,6 @@
-package com.pras.bareksatest.
+package com.pras.bareksatest.model.local.entity
+
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+impor
