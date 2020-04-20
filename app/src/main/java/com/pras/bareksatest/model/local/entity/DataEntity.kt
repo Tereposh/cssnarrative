371b40c12a73dd99ae68e8cba79ea84dda083f55
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-04-20 12:27:19 +0000

    Run scan-local.sh on phpmyadmin:...

--- app/src/main/java/com/pras/bareksatest/model/local/entity/DataEntity.kt
+++ app/src/main/java/com/pras/bareksatest/model/local/entity/DataEntity.kt
@@ -3,4 +3,8 @@ package com.pras.bareksatest.model.local.entity
 import android.os.Parcelable
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
-impor
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "
