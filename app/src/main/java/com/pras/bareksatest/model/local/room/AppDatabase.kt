c05727e55aa4567ef9d26143b4133972536eb411
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-05-11 21:53:12 +0000

    b43legacy: Fix firmware loading when driver is built into the kernel

--- app/src/main/java/com/pras/bareksatest/model/local/room/AppDatabase.kt
+++ app/src/main/java/com/pras/bareksatest/model/local/room/AppDatabase.kt
@@ -5,4 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-impo
+import com.pras.bareksatest.model.local.entity.DataEntity
+
+@Database(
+    entities = [DataEntity::clas
