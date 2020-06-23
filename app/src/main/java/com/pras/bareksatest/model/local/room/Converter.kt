c055f9657d281e3219fb39670d1834faf17dec43
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-06-23 14:34:16 +0000

    Updating info

--- app/src/main/java/com/pras/bareksatest/model/local/room/Converter.kt
+++ app/src/main/java/com/pras/bareksatest/model/local/room/Converter.kt
@@ -1,3 +1,5 @@
 package com.pras.bareksatest.model.local.room
 
-import androidx.
+import androidx.room.TypeConverter
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
