650f644ca25e835abd7412b8943b56c8b3526f4e
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-06-25 08:40:31 +0000

    merp

--- app/src/main/java/com/pras/bareksatest/model/local/room/Converter.kt
+++ app/src/main/java/com/pras/bareksatest/model/local/room/Converter.kt
@@ -3,3 +3,8 @@ package com.pras.bareksatest.model.local.room
 import androidx.room.TypeConverter
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class Converter {
+    @TypeConverter
+    fun fromList(value
