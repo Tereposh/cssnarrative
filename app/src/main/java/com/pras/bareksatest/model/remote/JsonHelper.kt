9d3919f6efae9878356d0c3d116b88bc99db6cfc
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-07-06 22:51:45 +0000

    chore(package): update ava to version 0.15.0

--- app/src/main/java/com/pras/bareksatest/model/remote/JsonHelper.kt
+++ app/src/main/java/com/pras/bareksatest/model/remote/JsonHelper.kt
@@ -2,4 +2,8 @@ package com.pras.bareksatest.model.remote
 
 import android.content.Context
 import android.util.Log
-import com.pras.bareksatest.model.remote.respon
+import com.pras.bareksatest.model.remote.response.DataResponse
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import kot
