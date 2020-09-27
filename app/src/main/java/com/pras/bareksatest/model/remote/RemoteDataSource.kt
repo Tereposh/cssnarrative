e47fb1f1912285fdbe8b5bdbbefe672682e4287b
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-09-27 09:22:23 +0000

    Remove stale comment

--- app/src/main/java/com/pras/bareksatest/model/remote/RemoteDataSource.kt
+++ app/src/main/java/com/pras/bareksatest/model/remote/RemoteDataSource.kt
@@ -4,4 +4,6 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.LiveData
-import andr
+import androidx.lifecycle.MutableLiveData
+import com.pras.bareksatest.model.remote.response.DataResponse
+import com.p
