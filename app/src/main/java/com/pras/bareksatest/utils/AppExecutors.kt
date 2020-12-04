511c721def79beb28ae731faa37222037f1599dd
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-12-04 06:02:01 +0000

    fix pagination

--- app/src/main/java/com/pras/bareksatest/utils/AppExecutors.kt
+++ app/src/main/java/com/pras/bareksatest/utils/AppExecutors.kt
@@ -4,4 +4,8 @@ import android.os.Handler
 import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
-import java.util.concurr
+import java.util.concurrent.Executors
+
+class AppExecutors@VisibleForTesting constructor(
+    private val diskIO: Executor,
+  
