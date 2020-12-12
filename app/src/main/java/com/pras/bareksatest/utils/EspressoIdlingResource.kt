f49b130ab53e6673f496e8741040a3b879be5c65
Tereposh <93296633+Tereposh@users.noreply.github.com>
2020-12-12 07:11:50 +0000

    Update index.Rmd

--- app/src/main/java/com/pras/bareksatest/utils/EspressoIdlingResource.kt
+++ app/src/main/java/com/pras/bareksatest/utils/EspressoIdlingResource.kt
@@ -3,4 +3,6 @@ package com.pras.bareksatest.utils
 import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.idling.CountingIdlingResource
 
-object EspressoIdlingResou
+object EspressoIdlingResource {
+    private val RESOURCE: String? = "GLOBAL"
+    private val espressoTestIdlingResource = Countin
