7f98f1b5db6893de54d726bd3fefcdc7ef1f64e0
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-01-01 21:00:44 +0000

    add debug print

--- app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
@@ -3,4 +3,8 @@ package com.pras.bareksatest.view
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Vie
+import android.view.View
+import android.view.ViewGroup
+import com.pras.bareksatest.R
+
+// TODO: Rename parameter arguments, choose names that mat
