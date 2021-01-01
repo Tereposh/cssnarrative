f7a7012e2edaf677a244f6ca577ab3c2e65966f8
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-01-01 22:50:20 +0000

    Adding missing docblock parameters

--- app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
@@ -7,4 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.pras.bareksatest.R
 
-// TODO: Rename parameter arguments, choose names that mat
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+privat
