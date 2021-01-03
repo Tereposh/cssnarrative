20ab92b86576c4b4b605c3997d9a09af324bc666
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-01-03 13:24:34 +0000

    Forward port zendframework/zf2#2904

--- app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
@@ -20,4 +20,7 @@ private const val ARG_PARAM2 = "param2"
 class DetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private var par
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedIns
