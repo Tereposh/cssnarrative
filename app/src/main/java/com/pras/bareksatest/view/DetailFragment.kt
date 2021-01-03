7c68c6ae43f7008105781f8ad9a46e72d56feb8e
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-01-03 12:12:14 +0000

    Updating configuration

--- app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
@@ -18,4 +18,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
-    // TOD
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var par
