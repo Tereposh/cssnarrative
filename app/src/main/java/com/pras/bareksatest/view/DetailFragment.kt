0c22c1610739ea7684f5ee071d65517f0eb3f796
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-01-03 08:25:51 +0000

    Small documentation fix.

--- app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/DetailFragment.kt
@@ -10,4 +10,8 @@ import com.pras.bareksatest.R
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
-privat
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInsta
