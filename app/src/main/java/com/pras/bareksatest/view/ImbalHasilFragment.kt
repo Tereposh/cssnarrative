33e6a679c2feeedcc75a72e3d9fb7c0d52d6682a
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-04-09 17:23:57 +0000

    updated gruntfile

--- app/src/main/java/com/pras/bareksatest/view/ImbalHasilFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/ImbalHasilFragment.kt
@@ -85,4 +85,6 @@ class ImbalHasilFragment : Fragment() {
                                 .name(it.data?.get(1)?.name)
                                 .data(arrayOf(0.0, 5.0, 10.0, 15.0, 25.0))
                                 .color("#67568B"),
-                            AASeriesElem
+                            AASeriesElement()
+                                .name(it.data?.get(2)?.name)
+                                .data(arrayOf
