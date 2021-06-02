b29aeec4d7d7b97c4914d69e193994f1d6c0a3cb
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-06-02 08:57:09 +0000

    [de] avoid false alarms

--- app/src/main/java/com/pras/bareksatest/view/ImbalHasilFragment.kt
+++ app/src/main/java/com/pras/bareksatest/view/ImbalHasilFragment.kt
@@ -140,4 +140,8 @@ class ImbalHasilFragment : Fragment() {
     companion object {
         /**
          * Use this factory method to create a new instance of
-         * this fragment using the provided pa
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new
