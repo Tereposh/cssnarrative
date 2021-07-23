345ce8b417b11759e171bec14f1570a7da70afb4
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-07-23 20:59:29 +0000

    yarn.lock

--- app/src/main/java/com/pras/bareksatest/vo/Resource.kt
+++ app/src/main/java/com/pras/bareksatest/vo/Resource.kt
@@ -2,4 +2,6 @@ package com.pras.bareksatest.vo
 
 data class Resource<T>(val status: Status, val data: T?, val message: String?) {
     companion object {
-        f
+        fun <T> success(data: T?): Resource<T> = Resource(Status.SUCCESS, data, null)
+
+        fun <T> error(msg: String?, data:
