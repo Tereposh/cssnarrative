4b299c782de39dfaf3c6d80cfa99f84fda1d8025
Tereposh <93296633+Tereposh@users.noreply.github.com>
2021-07-26 13:16:10 +0000

    purge

--- app/src/main/java/com/pras/bareksatest/vo/Resource.kt
+++ app/src/main/java/com/pras/bareksatest/vo/Resource.kt
@@ -4,4 +4,6 @@ data class Resource<T>(val status: Status, val data: T?, val message: String?) {
     companion object {
         fun <T> success(data: T?): Resource<T> = Resource(Status.SUCCESS, data, null)
 
-        fun <T> error(msg: String?, data:
+        fun <T> error(msg: String?, data: T?): Resource<T> = Resource(Status.ERROR, data, msg)
+
+        fun <T> loading(data: T?): Resource<T> = Reso
